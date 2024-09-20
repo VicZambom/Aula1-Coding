2fac09ee463747832dc15aded8ab882d932b80c1
VicZambom <140662019+VicZambom@users.noreply.github.com>
2024-09-20 14:12:12 -0300

    Update tiposPrimitivos.js

--- tiposPrimitivos.js
+++ tiposPrimitivos.js
@@ -1,1 +1,6 @@
-
+const prompt = require('prompt-sync')();
+let nota1 = parseInt(prompt("insira a nota 1: "))                        
+let nota2 = 9;
+let nota3 = 5;
+let media = ((nota1 + nota2 + nota3)/3). toFixed()
+console.log(media)
